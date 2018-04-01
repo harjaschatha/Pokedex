5e44feb22c1bde310d8dbf1bf565c8bd228c6f60
harjaschatha <harjaschatha@gmail.com>
2018-04-01 22:10:25 +0530

    Add urls for movelist

--- the_pokemon_project/urls.py
+++ the_pokemon_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   	path('', include('pokedex.urls')),
+   	path('pokemon/', include('pokedex.urls')),
+   	path('movelist/', include('moves.urls')),
 ]
